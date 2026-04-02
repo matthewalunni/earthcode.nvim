777f0cfe5a58ffb0be2d8586a2c2222e3bb87fcf
matthewalunni <matthew.alunni@gmail.com>
2026-04-01 22:03:51 -0400

    fix: multitab overlay issue

--- lua/earthcode/integrations/bufferline.lua
+++ lua/earthcode/integrations/bufferline.lua
@@ -3,14 +3,14 @@ local M = {}
 function M.load(c)
   local function hi(name, opts) vim.api.nvim_set_hl(0, name, opts) end
   hi("BufferLineFill",                { bg = c.bg })
-  hi("BufferLineBackground",          { fg = c.comment,  bg = c.ui_dark })
+  hi("BufferLineBackground",          { fg = c.comment,  bg = c.cursorline })
   hi("BufferLineSelected",            { fg = c.fg,       bg = c.bg,      bold = true })
   hi("BufferLineIndicatorSelected",   { fg = c.keyword,  bg = c.bg })
-  hi("BufferLineSeparator",           { fg = c.ui_mid,   bg = c.ui_dark })
+  hi("BufferLineSeparator",           { fg = c.ui_mid,   bg = c.cursorline })
   hi("BufferLineSeparatorSelected",   { fg = c.ui_mid,   bg = c.bg })
-  hi("BufferLineModified",            { fg = c.warning,  bg = c.ui_dark })
+  hi("BufferLineModified",            { fg = c.warning,  bg = c.cursorline })
   hi("BufferLineModifiedSelected",    { fg = c.warning,  bg = c.bg })
-  hi("BufferLineCloseButton",         { fg = c.comment,  bg = c.ui_dark })
+  hi("BufferLineCloseButton",         { fg = c.comment,  bg = c.cursorline })
   hi("BufferLineCloseButtonSelected", { fg = c.error,    bg = c.bg })
 end
 
